caa4dd1594c9826ad05fb10a3bae0680da9bf87f
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2022-08-31 03:19:10 +0300

    init

---
+++ components/Cover.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="d-flex justify-center align-center page-cover">
+    <video-background
+      :src="require('@/assets/bg.webm')"
+      :poster="require('@/assets/cover.jpg')"
+      overlay="linear-gradient(45deg,#2a4ae430,#fb949e6b)"
+      style="width: 100%; height: 100vh"
+    >
+      <v-container>
+        <div class="text-h1 text-md-h2 font-weight-light cover font-weight-bold">
+          <div>
+            <a style="font-size: 18px; letter-spacing: 1.5px" href="#design" class="font-weight-light">Наши услуги</a>
+          </div>
+          <div class="mb-6"><a href="#mobile">Мобильная разработка</a></div>
+          <div class="mb-6"><a href="#webdev">Создание веб-приложений</a></div>
+          <div class="mb-6"><a href="#support">Техническая поддержка</a></div>
+        </div>
+      </v-container>
+    </video-background>
+  </section>
+</template>
+<style lang="sass" scoped>
+.cover
+  margin: auto
+  width: fit-content
+  a
+    color: #fff
+    text-decoration: none
+</style>
---
+++ components/Presentation.vue
@@ -0,0 +1,14 @@
+<template>
+  <section id="presentation" class="secondary py-16 d-flex align-center text-md-center">
+    <v-container class="py-16">
+      <h4 class="text-h3 mb-3 font-weight-bold">Разработка сайтов, мобильных приложений, техническая поддержка</h4>
+      <h5 class="text-h4">
+        Имея 13 лет опыта в разработке за плечами, мы готовы взяться за выполнение самых сложных и амбициозных задач.
+      </h5>
+    </v-container>
+  </section>
+</template>
+<style lang="sass" scoped>
+.v-application .secondary
+  background-color: rgba(59, 59, 59, 0.8) !important
+</style>
